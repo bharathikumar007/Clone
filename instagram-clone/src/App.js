b723f98697d3bf1ade955df3b70529616296d55e
bharathikumar <rbharathikumar27@gmail.com.com>
2020-09-15 01:12:55 +0530

    First Commit

---
+++ instagram-clone/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Instagram Clone</h1>
+
+      <div className="app__header">
+        <p>Going to make this some time</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
